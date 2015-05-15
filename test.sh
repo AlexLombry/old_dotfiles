c1cfdda334a043385f7d1fe85c885b4c47cfe4c3
Alex Lombry <alex.lombry@gmail.com>
2015-05-16 00:55:10 +0200

    change configuration style

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ echo "Creating all the symlinks"
 linkables=$( ls -1 -d *.lnk )
 
 for file in $linkables ; do
-target="$HOME/.$( basename $file ".lnk" )"
+    target="$HOME/.$( basename $file ".lnk" )"
     echo "Create link for $file"
     ln -s $DOTFILES/$file $target
     echo ""
