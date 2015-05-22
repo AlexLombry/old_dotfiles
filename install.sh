cae0be62231589ceb2422d5865d12d244f89d006
Alex Lombry <alex.lombry@gmail.com>
2015-05-22 21:50:16 +0200

    update and cleanup

--- install.sh
+++ install.sh
@@ -6,16 +6,19 @@ if [ "$(uname)" == "Darwin" ]; then
     echo "running on OSX"
 
     echo "install software for mac"
-    source install/software.sh
+    #source install/software.sh
 
     echo "install brew and all things"
-    source install/brew.sh
+    #source install/brew.sh
+
+    echo "generate all symlinks"
+    source install/link.sh
 
     echo "updating macosx configuration"
     source install/osx.sh
 
-    echo "generate all symlinks"
-    source install/link.sh
+    echo "Cloning Vundle for vim"
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 echo "configuring zsh as default shell"
