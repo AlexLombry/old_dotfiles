779807471f69584611be178ddfa0eef96c8751e4
Alex Lombry <alex.lombry@gmail.com>
2015-05-29 18:46:48 +0200

    update link file

--- install/link.sh
+++ install/link.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 DOTFILES=$HOME/dotfiles
+BACKUP_TIME=$(date +%s)
 
 echo "creating symlinks"
+
 linkables=$( ls -1 -d **/*.lnk )
+
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".lnk" )"
     echo "Backup your old file"
-    cp $target $target.mybackup
+    mv $target $target.backup.$BACKUP_TIME
 
     echo "Create link for $file"
     ln -s $DOTFILES/$file $target
