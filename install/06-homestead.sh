acfd2fe55c4f72a11083d89631cd269c62ebd600
Alex Lombry <alex.lombry@gmail.com>
2016-02-02 20:44:54 +0100

    fix homestead script

--- install/06-homestead.sh
+++ install/06-homestead.sh
@@ -19,10 +19,12 @@ echo "Global require of homestead"
 composer global require "laravel/homestead=~2.0"
 
 echo "Add composer bin path into your zshrc file"
-echo "~/.composer/vendor/bin:$PATH" >> ~/.zshrc
+echo "export PATH=\"~/.composer/vendor/bin:\$PATH"\" >> ~/.zshrc
 
 echo "Source your zshrc file and init homestead"
 source ~/.zshrc
 ~/.composer/vendor/bin/homestead init
 
-echo "You have to update you .homestead/Homestead.yaml file and then do a homestead up"
+echo "You have to update your .homestead/Homestead.yaml file and then do a homestead up"
+
+
