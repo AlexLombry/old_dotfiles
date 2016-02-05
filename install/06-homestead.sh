94822ecee9a0a0fc710663ff2611d0b7918283fc
Alex Lombry <alex.lombry@gmail.com>
2016-02-05 20:45:54 +0100

    update homestead script

--- install/06-homestead.sh
+++ install/06-homestead.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-# Update brew
-echo "Updating brew"
-brew update
-
-# install virtualbox and vagrant from cask
-echo "Install virtualbox and vagrant if not exist"
-
-# Set your personnal computer name
-read -p "Installation de Virtualbox et Vagrant (y/n) " RESP
-if [ "$RESP" = "y" ]; then
-	brew cask install virtualbox vagrant    
-else
-    echo "Skip this step"
-fi
+echo "Installation of composer"
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
 
 echo "Global require of homestead"
 composer global require "laravel/homestead=~2.0"
 
 echo "Add composer bin path into your zshrc file"
-echo "export PATH=\"~/.composer/vendor/bin:\$PATH"\" >> ~/.zshrc
+echo "\nexport PATH=\"/Users/$USER/.composer/vendor/bin:\$PATH"\" >> ~/.zshrc
 
 echo "Source your zshrc file and init homestead"
 source ~/.zshrc
-~/.composer/vendor/bin/homestead init
-
-echo "You have to update your .homestead/Homestead.yaml file and then do a homestead up"
