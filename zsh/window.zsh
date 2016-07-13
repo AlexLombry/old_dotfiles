7741f40f2cf4a6253f2ce67d4be4119fcc73cbcd
Alex Lombry <alex.lombry@gmail.com>
2016-07-13 15:24:09 +0200

    delete comment

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -1,5 +1,4 @@
 # From http://dotfiles.org/~_why/.zshrc
-# Sets the window title nicely no matter where you are
 function title() {
   # escape '%' chars in $1, make nonprintables visible
   a=${(V)1//\%/\%\%}
