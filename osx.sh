3a2c5caf79e9ad2da5e79ee9b2d8276def29f420
Alex Lombry <alex.lombry@gmail.com>
2016-09-20 14:38:09 +0200

    update osx

--- osx.sh
+++ osx.sh
@@ -34,54 +34,71 @@ if [ "$RESP" = "y" ]; then
 else
     running "Skip this step"
 fi
+ok
 
 running "Finder: show all filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+ok
 
 running "not showing hidden files by default"
 defaults write com.apple.Finder AppleShowAllFiles -bool false
+ok
 
 running "only use UTF-8 in Terminal.app"
 defaults write com.apple.terminal StringEncodings -array 4
+ok
 
 running "Expand save dialog by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+ok
 
 running "Show the ~/Library folder in Finder"
 chflags nohidden ~/Library
+ok
 
 running "Use current directory as default search scope in Finder"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+ok
 
 running "Show Path bar in Finder"
 defaults write com.apple.finder ShowPathbar -bool true
+ok
 
 running "Disable autohide for the Dock"
 defaults write com.apple.dock autohide -bool false
+ok
 
 running "Show Status bar in Finder"
 defaults write com.apple.finder ShowStatusBar -bool true
+ok
 
 running "Set a blazingly fast keyboard repeat rate"
 defaults write NSGlobalDomain KeyRepeat -int 2
+ok
 
 running "Set a shorter Delay until key repeat"
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
+ok
 
 #running "Disable tap to click (Trackpad)"
 #defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool false
+#ok
 
 running "Enable Safari’s debug menu"
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+ok
 
 running "Disable smart quotes as it’s annoying for messages that contain code"
 defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticQuoteSubstitutionEnabled" -bool false
+ok
 
 running "Disable natural Lion-style scrolling"
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+ok
 
 running "Kill affected applications"
 for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
+ok
 
 ###############################################################################
 # SSD-specific tweaks                                                         #
@@ -104,11 +121,14 @@ if [ "$RSSD" = "y" ]; then
     sudo chflags uchg /Private/var/vm/sleepimage;ok
 else
     running "Ok, let's move on"
+    ok
 fi
 ###############################################################################
 # Homebrew sweets                                                             #
 ###############################################################################
 running "checking homebrew install"
+ok
+
 brew_bin=$(which brew) 2>&1 > /dev/null
 if [[ $? != 0 ]]; then
     action "installing homebrew"
