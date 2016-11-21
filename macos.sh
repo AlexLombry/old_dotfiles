2332fa5f423b69175af7f1e11cb16c8e79b3cf7d
Alex Lombry <alex.lombry@gmail.com>
2016-11-21 17:35:26 +0100

    Composer installer

--- macos.sh
+++ macos.sh
@@ -225,3 +225,16 @@ bot "Alright, cleaning up homebrew cache..."
 # Remove outdated versions from the cellar
 brew cleanup > /dev/null 2>&1
 bot "All clean"
+
+bot "Installing composer..."
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'aa96f26c2b67226a324c27919f1eb05f21c248b987e6195cad9690d5c1ff713d53020a02ac8c217dbf90a7eacc9d141d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
+/usr/local/bin/composer global require friendsofphp/php-cs-fixer
+/usr/local/bin/composer global require laravel/valet
+/usr/local/bin/composer global require m6web/coke
+/usr/local/bin/composer global require laravel/installer
+/usr/local/bin/composer global require laravel/lumen-installer
