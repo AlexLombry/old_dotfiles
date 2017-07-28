045f764904852e482bca259d7e092352fd4d8324
Alex Lombry <alex.lombry@gmail.com>
2017-07-28 23:35:56 +0200

    Update developer minimal stuff and rename run first

--- developer.sh
+++ developer.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
 source ./lib.sh
 
-bot "This script install Composer, Cocoapods, Pip"
+bot "updating homebrew"
+    /usr/local/bin/composer global require friendsofphp/php-cs-fixer
+    /usr/local/bin/composer global require laravel/valet
+    /usr/local/bin/composer global require m6web/coke
+    /usr/local/bin/composer global require laravel/installer
+    /usr/local/bin/composer global require laravel/lumen-installer
 ok
 
-bot "Installing composer..."
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === 'aa96f26c2b67226a324c27919f1eb05f21c248b987e6195cad9690d5c1ff713d53020a02ac8c217dbf90a7eacc9d141d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
+bot "updating homebrew"
+    brew update
+ok
 
-/usr/local/bin/composer global require friendsofphp/php-cs-fixer
-/usr/local/bin/composer global require laravel/valet
-/usr/local/bin/composer global require m6web/coke
-/usr/local/bin/composer global require laravel/installer
-/usr/local/bin/composer global require laravel/lumen-installer
+bot "Create vim swap and backup dir"
+    rm -rf ~/.vim
+    require_brew macvim --override-system-vim --with-cscope --with-lua --HEAD
+    require_brew vim --env-std --override-system-vim --with-lua
 
+    mkdir ~/.vim/backups
+    mkdir ~/.vim/swap
+    mkdir ~/.vim/bundle
+    mkdir ~/.vim/colors
+    mkdir ~/.vim/autoload
+    curl https://raw.githubusercontent.com/farfanoide/vim-facebook/master/colors/facebook.vim -o ~/.vim/colors/facebook.vim
+    curl https://raw.githubusercontent.com/gosukiwi/vim-atom-dark/master/colors/atom-dark-256.vim -o ~/.vim/colors/atom-dark-256.vim
+ok
+
+bot "Cloning Vundle for vim"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ok
+
+bot "Launch Vim and configuring it"
+    vim +PluginInstall +qall
+ok
