706836d1e4abb263e350438bbd74aca907cc04ae
Alex Lombry <alex.lombry@gmail.com>
2018-10-17 16:02:00 +0200

    Reinstall powerline

--- shell/software.sh
+++ shell/software.sh
@@ -49,9 +49,10 @@ ok
 
 running "Install Powerline shell"
 mkdir -p ~/Code/
-git clone https://github.com/banga/powerline-shell
+git clone https://github.com/b-ryan/powerline-shell
 cd powerline-shell
 sudo python setup.py install
+mkdir -p ~/.config/powerline-shell && powerline-shell --generate-config > ~/.config/powerline-shell/config.json
 ok
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
