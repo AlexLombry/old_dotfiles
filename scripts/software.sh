75c00162bc4decec491faff7762fb81c03380c2f
Alex Lombry <alex.lombry@gmail.com>
2019-02-14 10:08:39 +0100

    Update zshrc

--- scripts/software.sh
+++ scripts/software.sh
@@ -55,4 +55,8 @@ ok
 # mkdir -p ~/.config/powerline-shell && powerline-shell --generate-config > ~/.config/powerline-shell/config.json
 # ok
 
+running "Install Zsh Plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ok
