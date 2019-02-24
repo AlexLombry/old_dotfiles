7d7dc07a04de460ee078e7f4381d582a10af3b01
Alex Lombry <alex.lombry@gmail.com>
2019-02-24 21:08:42 +0100

    Update macos script

---
+++ scripts/debug.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+source ~/dotfiles/scripts/lib.sh
+
+ok "toto"
+bot "toto"
+running "toto"
+action "toto"
+warn "toto"
+error "toto"
--- scripts/macos.sh
+++ scripts/macos.sh
@@ -43,24 +43,24 @@ running "Use current directory as default search scope in Finder"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 ok
 
-running "Show Path bar in Finder"
-defaults write com.apple.finder ShowPathbar -bool true
-ok
-
-running "Enable autohide for the Dock"
-defaults write com.apple.dock autohide -bool true
-ok
-
-running "Show Status bar in Finder"
-defaults write com.apple.finder ShowStatusBar -bool true
+running "Disable autohide for the Dock"
+defaults write com.apple.dock autohide -bool false
 ok
 
 running "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain KeyRepeat -int 0
 ok
 
 running "Set a shorter Delay until key repeat"
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain InitialKeyRepeat -int 0
+ok
+
+running "Increase sound quality for Bluetooth headphones/headsets"
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+ok
+
+running "Disable auto correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 ok
 
 running "Enable tap to click (Trackpad)"
@@ -79,10 +79,16 @@ running "Disable natural Lion-style scrolling"
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 ok
 
-bot "Speed up Dock switching"
+running "Speed up Dock switching"
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
 killall Dock
+ok
+
+running "Automatically open a new Finder window when a volume is mounted"
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 ok
 
 # ==============================================
@@ -106,11 +112,15 @@ running "Finder: disable window animations and Get Info animations"
 defaults write com.apple.finder DisableAllAnimations -bool true
 ok
 
-running "Show icons for hard drives, servers, and removable media on the desktop"
+running "Show icons for servers, and removable media on the desktop"
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+ok
+
+running "Show Path bar in Finder"
+defaults write com.apple.finder ShowPathbar -bool true
 ok
 
 running "Finder: show all filename extensions"
@@ -157,6 +167,48 @@ defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
 ok
 
 bot "Please run command defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText \"LOCK MESSAGE\""
+
+running "Privacy: don’t send search queries to Apple"
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+ok 
+
+running "Set Safari’s home page to about:blank for faster loading"
+defaults write com.apple.Safari HomePage -string "about:blank"
+ok
+
+running "Prevent Safari from opening ‘safe’ files automatically after downloading"
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+ok
+
+running "Copy email addresses as foo@example.com instead of Foo Bar <foo@example.com> in Mail.app"
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+ok
+
+running "Display emails in threaded mode, sorted by date (oldest at the top)"
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
+ok
+
+running "Disable inline attachments (just show the icons)"
+defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+ok 
+
+running "Disable automatic spell checking"
+defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
+ok 
+
+running "Only use UTF-8 in Terminal.app"
+defaults write com.apple.terminal StringEncodings -array 4
+ok
+
+running "Prevent Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+ok
+
+running "Disable apple sound beep feedback"
+defaults write "com.apple.sound.beep.feedback" -int 1
 ok
 
 # ==============================================
