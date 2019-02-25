d1063aa9f8d28688343c2ebf63f5ce0d62d41522
Alex Lombry <alex.lombry@gmail.com>
2019-02-25 16:04:23 +0100

    App unidentified

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -14,6 +14,10 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+running "Allow any Application to be installed"
+sudo spctl --master-disable
+ok
+
 running "Finder: show all filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 ok
