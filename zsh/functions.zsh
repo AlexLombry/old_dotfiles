b0d9a5b1f14a49b0f7973561cc86ee54894d7123
Alex <alex.lombry@gmail.com>
2019-05-08 22:25:30 +0200

    Update func, color

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -360,5 +360,5 @@ function stopdev() {
 function destroy_docker() {
     docker kill $(docker ps -q);
     docker rm $(docker ps -a -q); 
-    docker rmi $(docker images -q)
+    # docker rmi $(docker images -q)
 }
