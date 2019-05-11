2805edeb2638bf005bb9f6cb3ca2b26ab0903541
Alex Lombry <alex.lombry@gmail.com>
2019-05-11 23:18:15 +0200

    Cleanup of aliases and function

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,9 +8,6 @@ else # OS X `ls`
     colorflag="-G"
 fi
 
-alias vzz="sublime ~/.zshrc"
-alias dot="cd ~/dotfiles; sublime ."
-
 # Filesystem aliases
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -20,35 +17,29 @@ alias .....="cd ../../../.."
 alias l="exa -abghl --git --color=automatic"
 alias la="exa -abghl --git --color=automatic"
 alias lla="exa -abghl --git --color=automatic"
-alias ll="ls -lFh ${colorflag}"
+alias ll="ls -laFh ${colorflag}"
 alias lld="ls -l | grep ^d"
-alias rmf="rm -rf"
 
 # Helpers
 alias grep='grep --color=auto'
-alias df='df -h' # disk free, in Gigabytes, not bytes
-alias du='du -h -c' # calculate disk usage for a folder
 
 # Applications
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 alias chrome="/usr/bin/open -a /Applications/Google\ Chrome.app"
 alias sublime="/usr/bin/open -a /Applications/Sublime\ Text.app"
-alias subl="/usr/bin/open -a /Applications/Sublime\ Text.app"
 alias vscode="/usr/bin/open -a /Applications/Visual\ Studio\ Code.app"
 
 # Network
 alias webip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias localip="ipconfig getifaddr en0"
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
-alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
 # Trim new lines and copy to clipboard
 alias trimcopy="tr -d '\n' | pbcopy"
 
 # Recursively delete `.DS_Store` files
-alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+alias dsstore_delete="find . -name '*.DS_Store' -type f -ls -delete"
 
 # File size
 alias fs="stat -f \"%z bytes\""
@@ -57,12 +48,12 @@ alias fs="stat -f \"%z bytes\""
 alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; rm -rfv ~/.Trash"
 
 # Show/hide hidden files in Finder
-alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+alias showfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hidefiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
 
-# Hide/show all desktop icons (useful when presenting)
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+# Show/hide all desktop icons (useful when presenting)
 alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
 
 # Change dock animation
 alias killdock="defaults delete com.apple.dock expose-animation-duration; killall Dock"
@@ -89,28 +80,24 @@ alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v exten
 
 # Divers
 alias loadsshkey="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'Copied to clipboard.'"
-alias restartApache="sudo apachectl restart"
-alias etcho="sudo vim /etc/hosts"
 alias checkphp="find -L . -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l"
 alias vimrc="vim ~/.vimrc"
 alias zrc="vim ~/.zshrc"
 alias :q="exit"
 
-alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'''
-
 # Wi-Fi Commands
 alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport"
-alias wificlean="sudo python ~/dotfiles/tools/cleanwifi_macos.py;"
-alias wifireboot="networksetup -setairportpower en0 off; networksetup -setairportpower en0 on"
-alias wifihardware="networksetup -listallhardwareports"
-alias wifiscan="airport scan"
-alias wifitake="networksetup -setairportnetwork en0 $1"
-alias ssid="airport -I|awk '/^ *SSID/ {print $2}'"
+alias wifi.clean="sudo python ~/dotfiles/tools/cleanwifi_macos.py;"
+alias wifi.reboot="networksetup -setairportpower en0 off; networksetup -setairportpower en0 on"
+alias wifi.hardware="networksetup -listallhardwareports"
+alias wifi.scan="airport scan"
+alias wifi.take="networksetup -setairportnetwork en0 $1"
+alias wifi.ssid="airport -I|awk '/^ *SSID/ {print $2}'"
 alias iface="networksetup -listallhardwareports|grep -A1 Wi-Fi|awk '/Device:/ {print $2}'"
 
 # iOS
-alias simulator="open -a Simulator.app --args -CurrentDeviceUDID $1"
-alias list-simulator="xcrun simctl list"
+alias simulator-run="open -a Simulator.app --args -CurrentDeviceUDID $1"
+alias simulator-list="xcrun simctl list"
 
 # Unix
 alias genpass="pwgen -CsB 26 1 | tr -d ' ' | tr -d '\n' | pbcopy"
@@ -120,31 +107,6 @@ alias -s log="less -MN"
 alias -s html="open"
 alias -s php=vim
 
-transfer() { 
-    if [ $# -eq 0 ]; then 
-        echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; 
-        return 1; 
-    fi 
-    tmpfile=$( mktemp -t transferXXX ); 
-    if tty -s; then 
-        basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); 
-        curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; 
-    else 
-        curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; 
-    fi; 
-
-    echo "\n\n";
-    cat $tmpfile; 
-    current=$( cat $tmpfile )
-    echo "\n\n";
-
-    open -a "Google Chrome" $current;
-
-    rm -f $tmpfile; 
-} 
-
-alias transfer=transfer
-
 alias mute="osascript -e 'set volume output muted true'"
 alias sup="osascript -e 'set volume 10'"
 alias zrsync="rsync -avzhP"
@@ -152,10 +114,8 @@ alias httpbench="wrk -t12 -c400 -d30s"
 
 # Show network connections
 # Often useful to prefix with SUDO to see more system level network usage
-alias network.connections='lsof -l -i +L -R -V'
-alias network.established='lsof -l -i +L -R -V | grep ESTABLISHED'
-alias network.internalip="ifconfig en0 | egrep -o '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)'"
-alias network.externalip='curl -s http://checkip.dyndns.org/ | sed "s/[a-zA-Z<>/ :]//g"'
+alias net.connections='lsof -l -i +L -R -V'
+alias net.established='lsof -l -i +L -R -V | grep ESTABLISHED'
 
 # Files used, anywhere on the filesystem
 alias files.usage='sudo fs_usage -e -f filesystem|grep -v CACHE_HIT|grep -v grep'
@@ -165,17 +125,12 @@ alias files.open='sudo fs_usage -e -f filesystem|grep -v CACHE_HIT|grep -v grep|
 alias files.usage.user='sudo fs_usage -e -f filesystem|grep -v CACHE_HIT|grep -v grep|grep Users'
 
 alias deletefilerecursive="find . -type f -print0 | xargs -0 rm"
-alias sshs="bash ~/dotfiles/tools/ssh-manager.sh"
 
 alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'''
-alias ifconfig-ext='curl ifconfig.me'
 alias dss='du -sck * | sort -n'
 
-alias emailgrep='grep -E -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b"'
 alias t='vendor/bin/phpunit'
 alias tf='vendor/bin/phpunit --filter $1'
-
-alias be='bundle exec'
 
 alias bubu="brew update && brew upgrade --all && brew cleanup"
 alias brewup='brew update; brew upgrade; brew prune; brew cleanup; brew doctor'
@@ -184,11 +139,8 @@ alias bsr="brew services restart $1"
 alias co="code ."
 
 alias dkc="docker-compose"
-alias dls="docker container ls"
-alias dlsa="docker container ls -a"
-alias dlsaq="docker container ls -qa"
-alias dstpoall="docker container stop $(docker container ls -qa)"
-alias drmall="docker container rm $(docker container ls -qa)"
+alias dk-stop="docker container stop $(docker container ls -qa)"
+alias dk-rmall="docker container rm $(docker container ls -qa)"
 
 function dshell() {
     (docker container exec -ti $1 bash) || (docker container exec -ti $1 sh);   
@@ -201,5 +153,3 @@ alias ng-lo="ngrok http $1 --subdomain=alombry"
 alias tojson="php ~/dotfiles/tools/toJson.php $1"
 
 alias laraclean="php artisan cache:clear; php artisan config:clear; php artisan optimize:clear; php artisan route:clear; php artisan view:clear;"
-
-alias backend="cd /Volumes/SecureDev/"
--- zsh/git.zsh
+++ zsh/git.zsh
@@ -5,22 +5,14 @@ alias gccc='git commit -m'
 alias gp="git push"
 alias gs="git status"
 alias gss="git status -sb"
-
 alias git-amend='git commit --amend -C HEAD'
 alias git-undo='git reset --soft HEAD~1'
 alias git-count='git shortlog -sn'
 alias git-undopush="git push -f origin HEAD^:master"
 alias nah="git reset --hard; git clean -df;"
 alias gcp="git checkout ."
-alias git-count-lines="git ls-files | xargs -n1 git blame --line-porcelain | gsed -n 's/^author //p' | sort -f | uniq -ic | sort -nr"
-
-# Work in progress
-function wip()
-{
-    git status
-    git add .
-    git commit -m "Work In Progress"
-}
+alias git-count-lines="git ls-files | xargs -n1 git blame --line-porcelain | sed -n 's/^author //p' | sort -f | uniq -ic | sort -nr"
+alias wip='git status && git add . && git commit -m "Work In Progress"'
 
 # git root
 function git-give-credit() {
@@ -32,32 +24,6 @@ function git-give-credit() {
 function git-rename() {
     git mv $1 "${2}-"
     git mv "${2}-" $2
-}
-
-function g() {
-    if [[ $# > 0 ]]; then
-        # if there are arguments, send them to git
-        git $@
-    else
-        # otherwise, run git status
-        git s
-    fi
-}
-
-function git-clean() {
-    if [[ $# > 0 ]]; then
-        git branch --merged | grep -v "\*" | grep -v "master" | grep -v "dev" | xargs -n 1 git branch -d
-    else
-        git branch --merged | grep -v "\*" | grep -v "master" | grep -v "dev"
-    fi
-}
-
-function gf() {
-    if [[ $# > 0 ]]; then
-        git flow $@
-    else
-        git flow log
-    fi
 }
 
 # Rename branches
