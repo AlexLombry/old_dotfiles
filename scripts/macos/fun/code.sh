078306b6c59052a6bfa06cbb0275288219510a47
Alex Lombry <alex.lombry@gmail.com>
2019-10-18 12:08:03 +0200

    delete sublime config

--- scripts/macos/fun/code.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source ~/.dotfiles/scripts/zsh/functions.zsh
-
-action "Create symlinks to get back your Sublime configuration from Dropbox"
-echo cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
-echo rm -rf User
-echo ln -s ~/Dropbox/Sublime/User
-ok
-
