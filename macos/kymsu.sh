f62fe890b097ba0ae94ac32f96f6aa7d21c51ddb
Alex Lombry <alex.lombry@manomano.com>
2020-03-15 13:31:25 +0100

    Kymsu

---
+++ macos/kymsu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+source ~/.dotfiles/zsh/functions.zsh
+
+running "Installation of Keep Your Mac Stuff Updated"
+mkdir -p "$HOME/.installation"
+[ ! -d "$HOME/.installation/kymsu/" ] && git clone git@github.com:AlexLombry/kymsu.git "$HOME/.installation/kymsu"
+cd "$HOME/.installation/kymsu" && ./install.sh
+ok
