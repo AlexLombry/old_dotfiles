bff67df143c6b21409d690e7c234c0082a3e5e3b
Alex Lombry <alex.lombry@manomano.com>
2021-10-28 19:36:06 +0200

    lsp diag

--- nvim/lua/alex/lspconfig.lua
+++ nvim/lua/alex/lspconfig.lua
@@ -83,6 +83,64 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 --   filetypes = { "typescript", "typescriptreact", "typescript.tsx" }
 -- }
 
+nvim_lsp.diagnosticls.setup {
+  on_attach = on_attach,
+  filetypes = { 'kotlin', 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'css', 'less', 'scss', 'markdown', 'pandoc' },
+  init_options = {
+    linters = {
+      eslint = {
+        command = 'eslint_d',
+        rootPatterns = { '.git' },
+        debounce = 100,
+        args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
+        sourceName = 'eslint_d',
+        parseJson = {
+          errorsRoot = '[0].messages',
+          line = 'line',
+          column = 'column',
+          endLine = 'endLine',
+          endColumn = 'endColumn',
+          message = '[eslint] ${message} [${ruleId}]',
+          security = 'severity'
+        },
+        securities = {
+          [2] = 'error',
+          [1] = 'warning'
+        }
+      },
+    },
+    filetypes = {
+      javascript = 'eslint',
+      javascriptreact = 'eslint',
+      typescript = 'eslint',
+      typescriptreact = 'eslint',
+    },
+    formatters = {
+      eslint_d = {
+        command = 'eslint_d',
+        args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
+        rootPatterns = { '.git' },
+      },
+      prettier = {
+        command = 'prettier',
+        args = { '--stdin-filepath', '%filename' }
+      }
+    },
+    formatFiletypes = {
+      css = 'prettier',
+      javascript = 'eslint_d',
+      javascriptreact = 'eslint_d',
+      json = 'prettier',
+      scss = 'prettier',
+      less = 'prettier',
+      typescript = 'eslint_d',
+      typescriptreact = 'eslint_d',
+      json = 'prettier',
+      markdown = 'prettier',
+    }
+  }
+}
+
 nvim_lsp.kotlin_language_server.setup {
     on_attach = on_attach,
     capabilities = capabilities
